956aba44cb1047673f8cd3f9ed4351443a8e6b93
Banfi Peter <p6t6rbanfi@gmail.com>
2018-03-03 18:55:34 +0100

    Fixed minor and major things

--- js/got.js
+++ js/got.js
@@ -24,15 +24,16 @@ function successAjax(xhttp) {
     */
     //createTable(characters)
     sortingDatas(characters)
+    console.log('heee');
     document.getElementById('search').addEventListener("change", function () {
         search(characters);
     });
     /*     document.getElementById('search').addEventListener("click", function () {
             empty();
         }); */
-    document.querySelector('.portraits').addEventListener("click", function () {
-        char(adat, characters);
-    });;
+    /*  document.querySelector('.portraits').addEventListener("click", function () {
+         char(adat, characters);
+     });; */
 }
 
 // Írd be a json fileod nevét/útvonalát úgy, ahogy nálad van
@@ -56,21 +57,53 @@ function sortingDatas(characters) {
             }
         }
     }
-    createTable(characters);
+    //createTable(characters);
+    char(characters);
 }
 
 function char(characters) {
     for (var i = 0; i < characters.length; i++) {
-        (function createContent(i) {
-            var el = document.createElement('p');
-            el.textContent = 'Click';
-            el.addEventListener('click', function () {
-                console.log(i);
-            });
-            document.body.appendChild(el);
-        })(i);
+        if (characters[i]["Halotte"] != 'true') {
+            (function createContent(i) {
+                var picture = document.createElement('img');
+                picture.src = characters[i]["Portrékép"];
+                var names = document.createElement('p');
+                names.textContent = characters[i]["Név"];
+
+                picture.addEventListener('click', function () {
+
+                    var input = characters[i]["Kép"];
+                    var parts = input.split('_');
+
+                    if (parts.length > 1) {
+                        var house = parts[1];
+                        var result = house.split(".");
+                        var houseName = result[0];
+                    }
+                    /* if (parts.length < 2) {
+                                            document.getElementById('house').style.display = "none";
+                                            console.log('hé');
+                                        } */
+                    document.querySelector('#result').innerHTML = '';
+                    var table = '';
+                    table += `
+                        <img src="${characters[i]["Kép"]}">
+                        <h4>${characters[i]["Név"]}</h4> 
+                        <img id="house" src="assets/houses/${houseName}.png">
+                        <p>${characters[i]["Történet"]}</p>
+                         `;
+                    table += '</div>';
+                    document.getElementById('rightInfo').innerHTML = table;
+
+
+                });
+                document.getElementById('firstRow').appendChild(picture);
+                document.getElementById('firstRow').appendChild(names);
+            })(i);
+        }
     }
 }
+
 
 function search(characters) {
     var input = document.querySelector('#search').value.toLowerCase();
@@ -98,7 +131,7 @@ function empty() {
     document.querySelector('#search').innerHTML = '';
 }
 
-function createTable(characters) {
+/* function createTable(characters) {
     var table = '';
 
     for (var i = 0; i < characters.length; i++) {
@@ -110,4 +143,4 @@ function createTable(characters) {
     }
     table += '</div> ';
     document.getElementById('firstRow').innerHTML = table;
-}
+} */
